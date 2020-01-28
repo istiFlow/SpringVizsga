dc0a25361e8860d49abe447c600e9952bf796875
Istvan Dumitras <d.steven@protonmail.ch>
2020-01-28 17:38:25 +0100

    m

--- musicstore_start/src/main/java/hu/flowacademy/musicstore/Controller/AlbumController.java
+++ musicstore_start/src/main/java/hu/flowacademy/musicstore/Controller/AlbumController.java
@@ -1,11 +1,47 @@
 package hu.flowacademy.musicstore.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.flowacademy.musicstore.Model.Album;
+import hu.flowacademy.musicstore.Model.Artist;
+import hu.flowacademy.musicstore.Service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class AlbumController {
 
+    @Autowired
+    private AlbumService albumService;
 
+    @PostMapping("/albums")
+    public ResponseEntity save(@RequestBody Album album){
+        albumService.createAlbum(album);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/albums")
+    public List<Album> findAll(){
+        return albumService.findAll();
+    }
+
+    @GetMapping("/albums/{id}")
+    public Album get(@PathVariable Long id ){
+        return albumService.getAlbum(id);
+    }
+
+    @PutMapping("/albums")
+    public ResponseEntity update(@RequestBody Album album){
+        albumService.updateAlbum(album);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/albums/{id}")
+    public ResponseEntity<Void> deleteArtists(@PathVariable Long id){
+        albumService.deleteAlbum(id);
+        return ResponseEntity.ok().build();
+    }
 }
