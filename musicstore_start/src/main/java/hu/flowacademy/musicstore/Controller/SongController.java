3eea03719e29934bada8a76d5ee96f1caf71b61a
Istvan Dumitras <d.steven@protonmail.ch>
2020-01-28 17:40:54 +0100

    .

--- musicstore_start/src/main/java/hu/flowacademy/musicstore/Controller/SongController.java
+++ musicstore_start/src/main/java/hu/flowacademy/musicstore/Controller/SongController.java
@@ -1,9 +1,48 @@
 package hu.flowacademy.musicstore.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.flowacademy.musicstore.Model.Artist;
+import hu.flowacademy.musicstore.Model.Song;
+import hu.flowacademy.musicstore.Service.ArtistService;
+import hu.flowacademy.musicstore.Service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class SongController {
+
+    @Autowired
+    private SongService songService;
+
+    @PostMapping("/songs")
+    public ResponseEntity save(@RequestBody Song song){
+        songService.createSong(song);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/songs")
+    public List<Song> findAll(){
+        return songService.findAll();
+    }
+
+    @GetMapping("/songs/{id}")
+    public Song get(@PathVariable Long id ){
+        return songService.getSong(id);
+    }
+
+    @PutMapping("/songs")
+    public ResponseEntity update(@RequestBody Song song){
+        songService.updateSong(song);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/songs/{id}")
+    public ResponseEntity<Void> deleteArtists(@PathVariable Long id){
+        songService.deleteSong(id);
+        return ResponseEntity.ok().build();
+    }
 }
